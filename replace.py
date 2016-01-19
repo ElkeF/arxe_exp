4846f1f34b44ae36dcf5df00dfdec53987eb8026
Elke Fasshauer <elke.fasshauer@uit.no>
2016-01-19 12:31:56 +0100

    add new cluster strutures and ICD efficiencies for them

---
+++ replace.py
@@ -0,0 +1,39 @@
+#! /opt/local/bin/python
+
+# IMPORT
+import os
+import sys
+import shutil
+import string
+import glob
+import math
+
+ls = os.listdir('.')
+ls.sort()
+
+count = 0
+
+rootdir = os.getcwd()
+#outfile = open('','w')
+
+for filename in glob.glob("*.tex"):
+#for filename in glob.glob("schale08.tex"):
+   print filename
+   infile = open(filename, 'r')
+   outfile = open('tmpfile', 'w')
+
+   
+#   replacements = {'includegraphics[scale=0.5]':'input', '/compl':'/schale', '/incompl':'/in-shell', '_':'-core', '.ps':'', 'caps':'cap'}
+   replacements = {'citenum':'cite'}
+
+   for line in infile:
+      if 'FloatBarrier' not in line:
+          for src, target in replacements.iteritems():
+              line = line.replace(src, target)
+          outfile.write(line)
+
+   infile.close()
+   outfile.close()
+
+   shutil.copyfile('tmpfile',filename)
+   os.remove('tmpfile')
